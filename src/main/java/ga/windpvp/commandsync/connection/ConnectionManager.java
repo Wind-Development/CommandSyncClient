2d739c4a5f0571ad480da950ab24ec18c37d9235
windcolor-dev <teaglehao@gmail.com>
2022-05-21 12:05:48 -0700

    Repeatedly try to connect if failed

--- src/main/java/ga/windpvp/commandsync/connection/ConnectionManager.java
+++ src/main/java/ga/windpvp/commandsync/connection/ConnectionManager.java
@@ -2,6 +2,8 @@ package ga.windpvp.commandsync.connection;
 
 import java.io.IOException;
 
+import org.bukkit.scheduler.BukkitRunnable;
+
 import ga.windpvp.commandsync.SyncPlugin;
 
 public class ConnectionManager {
@@ -11,12 +13,20 @@ public class ConnectionManager {
 	public ConnectionManager(SyncPlugin instance, int port, String name) {
 		connection = new Connection();
 
-		try {
-			connection.startConnection("127.0.0.1", port);
-			sendMessage("name " + name);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		// Continuously try to connect to the server
+		new BukkitRunnable() {
+			
+			@Override
+			public void run() {
+				try {
+					connection.startConnection("127.0.0.1", port);
+					sendMessage("name " + name);
+					cancel();
+				} catch (IOException ignored) {
+					instance.getLogger().warning("Unable to connect to the Velocity instance");
+				}
+			}
+		}.runTaskTimer(instance, 0, 40);
 	}
 
 	public void dispatchCommand(String command) {
